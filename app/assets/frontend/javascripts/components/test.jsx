1627c636f3c329ad996bdf3e392038bdba717d0f
aarkerio <mmontoya@gmail.com>
2016-02-08 11:25:02 -0600

    Update React component

---
+++ app/assets/frontend/javascripts/components/test.jsx
@@ -0,0 +1,17 @@
+//import React from 'react';
+//var React = require('react');
+// var React = require('react');
+//var ReactDOM = require('react-dom');
+
+//console.log('>>>> in test.jsx file');
+Component = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+// ReactDOM.render(<CommentBox />, document.getElementById('testdiv'));
