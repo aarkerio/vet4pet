89a44241c09b2766c469e6667221618180cc172a
aarkerio <mmontoya@gmail.com>
2016-02-17 13:29:15 -0600

    Update reduc call

--- app/assets/frontend/javascripts/entry.js
+++ app/assets/frontend/javascripts/entry.js
@@ -8,6 +8,12 @@ import App from './components/App'
 
 let my_store = createStore(todoApp)
 
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+let unsubscribe = my_store.subscribe(() =>
+  console.log(my_store.getState())
+)
+
 render(
   <Provider store={my_store}>
     <App />
