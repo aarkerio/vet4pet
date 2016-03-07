0c3fbf13f4d9fa0977f9126c18b99360b1bbfa15
jmmontoya <manuel@epublishing.com>
2016-03-07 09:38:21 -0600

    Adding Jets tests

---
+++ app/assets/frontend/javascripts/__tests__/testAppoComponent-spec.js
@@ -0,0 +1,30 @@
+jest.dontMock('../scripts/testComponent');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var CheckboxWithLabel = require('./scripts/testComponent');
+
+describe('CheckboxWithLabel', () => {
+
+  it('changes the text after click', () => {
+
+    // Render a checkbox with label in the document
+    var checkbox = TestUtils.renderIntoDocument(
+      <CheckboxWithLabel labelOn="On" labelOff="Off"/>
+    );
+
+    var checkboxNode = ReactDOM.findDOMNode(checkbox);
+
+    // Verify that it's Off by default
+    expect(checkboxNode.textContent).toEqual('Off');
+
+    // Simulate a click and verify that it is now On
+    TestUtils.Simulate.change(
+      TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+    );
+    expect(checkboxNode.textContent).toEqual('On');
+  });
+
+});
+
