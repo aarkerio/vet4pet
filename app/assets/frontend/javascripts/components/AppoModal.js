e0cdfd1e87dc78853d1d28b93b9e9853c94ee7fb
jmmontoya <manuel@epublishing.com>
2016-03-07 18:51:09 -0600

    Modal dialog fixed

--- app/assets/frontend/javascripts/components/AppoModal.js
+++ app/assets/frontend/javascripts/components/AppoModal.js
@@ -9,11 +9,13 @@ import { Button, Modal } from 'react-bootstrap';
 class AppoModal extends Component {
   constructor(props) {
       super(props);
-      console.log('MO22222 222222E!!!!!!!' + JSON.stringify(this.state));
       this.closeModal = this.closeModal.bind(this);
       this.state = { showModal: true };
+      console.log('MO22222 222222E!!!!!!!' + JSON.stringify(this.state));
   }
-
+  statics() {
+        console.log('I am static');
+  }
   closeModal() {
     this.setState({showModal: false});
     console.log('closeModal function state:'+ JSON.stringify(this.state));
@@ -26,7 +28,6 @@ class AppoModal extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    // this.props.onCommentSubmit(title_json,rank_json,year_json);
     // this.props.onRequestHide;
     return;
   }
@@ -50,40 +51,40 @@ class AppoModal extends Component {
       position: 'fixed',
       zIndex: 1040,
       top: 0, bottom: 0, left: 0, right: 0
-};
+    };
 
-const backdropStyle = {
-  ...modalStyle,
-  zIndex: 'auto',
-  backgroundColor: '#000',
-  opacity: 0.5
-};
+    const backdropStyle = {
+      ...modalStyle,
+      zIndex: 'auto',
+      backgroundColor: '#000',
+      opacity: 0.5
+    };
 
-const dialogStyle = function() {
-  // we use some psuedo random coords so modals
-  // don't sit right on top of each other.
-  let top = 50 + rand();
-  let left = 50 + rand();
+    const dialogStyle = function() {
+      // we use some psuedo random coords so modals
+      // don't sit right on top of each other.
+      let top = 50 + rand();
+      let left = 50 + rand();
 
-  return {
-    position: 'absolute',
-    width: 400,
-    top: top + '%', left: left + '%',
-    transform: `translate(-${top}%, -${left}%)`,
-    border: '1px solid #e5e5e5',
-    backgroundColor: 'white',
-    boxShadow: '0 5px 15px rgba(0,0,0,.5)',
-    padding: 20
-  };
-};
- return (
+      return {
+        position: 'absolute',
+        width: 400,
+        top: top + '%', left: left + '%',
+        transform: `translate(-${top}%, -${left}%)`,
+        border: '1px solid #e5e5e5',
+        backgroundColor: 'white',
+        boxShadow: '0 5px 15px rgba(0,0,0,.5)',
+        padding: 20
+      };
+    };
+    return (
         <div id="responsive" className="modal hide fade" tabIndex="-1" >
           <Modal aria-labelledby='modal-label'
             style={modalStyle}
             backdropStyle={backdropStyle}
             show={this.state.showModal}
-            onHide={this.closeModal}
-            onRequestHide={this.closeModal}
+            // onHide={this.closeModal}
+            // onRequestHide={this.closeModal}
           >
           <Modal.Header>
              <Modal.Title>Modal Überschrift Rot</Modal.Title>
@@ -116,8 +117,9 @@ AppoModal.propTypes = {
 
 function mapStateToProps(state, ownProps) {
   return {
-      id: ownProps.params.id
-  };
+      id: ownProps.params.id,
+      showModal: ownProps.showModal
+  }
 }
 
 export default connect(mapStateToProps)(AppoModal)
