c8cd6ae4390700b8a927de15a2a92eb001d2dccf
aarkerio <mmontoya@gmail.com>
2016-04-07 19:30:46 -0500

    Adding new mocha tests

---
+++ app/assets/frontend/javascripts/__tests__/components/appoModalComponent.js
@@ -0,0 +1,32 @@
+
+// file: test/component/todo-item.js
+var React = require('react/addons'),
+    assert = require('assert'),
+    TodoItem = require('../../common/components/todo-item'),
+    TestUtils = React.addons.TestUtils;
+
+describe('Todo-item component', function(){
+  before('render and locate element', function() {
+    var renderedComponent = TestUtils.renderIntoDocument(
+      <TodoItem done={false} name="Write Tutorial"/>
+    );
+
+    // Searching for <input> tag within rendered React component
+    // Throws an exception if not found
+    var inputComponent = TestUtils.findRenderedDOMComponentWithTag(
+      renderedComponent,
+      'input'
+    );
+
+    this.inputElement = inputComponent.getDOMNode();
+  });
+
+  it('<input> should be of type "checkbox"', function() {
+    assert(this.inputElement.getAttribute('type') === 'checkbox');
+  });
+
+  it('<input> should not be checked', function() {
+    assert(this.inputElement.checked === false);
+  });
+});
+
