d355080d360b0cfb6da0b0002da0a19d47534c3c
aarkerio <mmontoya@gmail.com>
2016-04-20 16:15:23 -0500

    Adding new flattener methods

--- lib/vendor/chipotle/flattener.rb
+++ lib/vendor/chipotle/flattener.rb
@@ -1,16 +1,30 @@
+#Chipotle Software (c) 2016 MIT License
+
 require 'nokogiri'
 require 'open-uri'
 
-
-doc = File.open("blossom.xml") { |f| Nokogiri::XML(f) }
-
 class Flattener
-
+  
+  def initialize(get_file)
+    @gt_file  = "./templates/Vital_Signs_Observations_GT.xml"
+    @xsd_file = './templates/Template.xsd'
+  end
+  
   def import
-    doc = Nokogiri::XML(File.open("../lib/templates/Vital_Signs_Observations_GT.xml")) do |config|
+    doc = Nokogiri::XML(File.open(@gt_file)) do |config|
       config.strict.nonet
     end
   end
 
+  def validate(root_element)
+    schema = Nokogiri::XML::Schema(File.read(schema_path))
+    document = Nokogiri::XML(File.read(document_path))
+    schema.validate(document.xpath("//#{root_element}").to_s)
+  end
+
+  def check_xsd
+    validate('input.xml', './templates/Template.xsd', 'container').each do |error|
+    puts error.message
+  end
 end
 
